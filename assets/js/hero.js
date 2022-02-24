7c04b6f8ed856a550b8234a9f0bc4434ba872df5
WebMobileDevelopment <maximmironenkoreactu@example.com>
2022-02-24 22:07:03 +0900

    about me added

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -86,3 +86,53 @@ for (var i = 0; i < navLinks.length; i++) {
     menuBtn.style.pointerEvents = "auto";
   });
 }
+
+// Image Lazy Loading
+window.addEventListener("DOMContentLoaded", function() {
+  var lazy = document.getElementsByClassName('lazy');
+  initializeLoad(lazy);
+});
+
+function initializeLoad(imgs) {
+  if (imgs.length > 0) {
+    if ('IntersectionObserver' in window) {
+      lazyLoad(imgs);
+    } else {
+      loadIntersection(imgs);
+    }
+  } else {
+    return;
+  }
+}
+
+function lazyLoad(lazy) {
+  var options = {
+    threshold: 0.8
+  }
+
+  var observer = new IntersectionObserver(function(entries, self) {
+    for (var i = 0; i < entries.length; i++) {
+      var entry = entries[i];
+      if (entry.isIntersecting) {
+        var src = entry.target.getAttribute('data-src');
+        TweenMax.set(entry.target, {attr:{src:src}});
+        TweenMax.fromTo(entry.target, 1, {css:{opacity:0, y:"-50px"}}, {css:{opacity:1, y:0}}, 1.5);
+        self.unobserve(entry.target);
+      }
+    }
+  }, options)
+  for (var i = 0; i < lazy.length; i++) {
+    var lazyItem = lazy[i];
+    observer.observe(lazyItem);
+  }
+}
+
+function loadIntersection(lazy) {
+  var io = document.createElement('script');
+  io.src = "https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver";
+  document.head.appendChild(io);
+  return io.onload = function() {
+    lazyLoad(lazy);
+  }
+}
+
