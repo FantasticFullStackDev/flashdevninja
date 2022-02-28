669b6bf08539bd9f721f6ca35c2971dbab449252
WebMobileDevelopment <maximmironenkoreactu@example.com>
2022-02-28 10:32:18 +0900

    skills service letter animation added

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -219,12 +219,12 @@ var letterLoad = {
 
 $(window).scroll(function(e){
   value = $(window).scrollTop();
-//   console.log(value);
-  if(value > 780 && !letterLoad[".one span"])
+  console.log(value);
+  if(value > 1200 && !letterLoad[".one span"])
     letterLoading(".one span");
-  if (value > 1500 && !letterLoad[".two span"])
+  if (value > 1700 && !letterLoad[".two span"])
     letterLoading(".two span");
-  if (value > 2200 && !letterLoad[".three span"])
+  if (value > 2100 && !letterLoad[".three span"])
     letterLoading(".three span");
 });
 
@@ -259,3 +259,85 @@ function letterLoading (target) {
       stagger: 0.05
   }, 0);
 }
+
+// Contact ME Animation
+const elts = {
+    text1: document.getElementById("text1"),
+    text2: document.getElementById("text2")
+};
+
+const texts = [
+    "If You're",
+    "Interested",
+    "Please Contact"
+];
+
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+    morph -= cooldown;
+    cooldown = 0;
+
+    let fraction = morph / morphTime;
+
+    if (fraction > 1) {
+        cooldown = cooldownTime;
+        fraction = 1;
+    }
+
+    setMorph(fraction);
+}
+
+function setMorph(fraction) {
+    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    fraction = 1 - fraction;
+    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    elts.text1.textContent = texts[textIndex % texts.length];
+    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+    morph = 0;
+
+    elts.text2.style.filter = "";
+    elts.text2.style.opacity = "100%";
+
+    elts.text1.style.filter = "";
+    elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    let newTime = new Date();
+    let shouldIncrementIndex = cooldown > 0;
+    let dt = (newTime - time) / 1000;
+    time = newTime;
+
+    cooldown -= dt;
+
+    if (cooldown <= 0) {
+        if (shouldIncrementIndex) {
+            textIndex++;
+        }
+
+        doMorph();
+    } else {
+        doCooldown();
+    }
+}
+
+animate();
